af4bd37533ff5604673240963f9e3aa66327eeb2
Champdinno <champdinno@gmail.com>
2025-07-31 04:03:58 +0700

    Update App.js

--- mtg-client/src/App.js
+++ mtg-client/src/App.js
@@ -1,38 +1,36 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import CardSearch from "./components/CardSearch";
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/Login'
+import Login from './components/Login';
 
 class App extends Component {
   constructor() {
-
-    super()
-
+    super();
     this.state = {
       randomCard: null,
-      isUpdated:false,
-      }
+      isUpdated: false,
+    };
   }
 
   componentDidMount() {
-    this.getRandomCard()
+    this.getRandomCard();
   }
 
   handleTextBoxChange = (e) => {
     this.setState({
-        [e.target.name]: e.target.value
-    })
+      [e.target.name]: e.target.value,
+    });
   }
 
   getRandomCard = () => {
-    let url = `https://api.scryfall.com/cards/random`
+    let url = `https://api.scryfall.com/cards/random`;
     fetch(url)
-    .then(response => response.json())
-    .then(json => {
-        console.log(json)
-      this.setState({randomCard: json})
-    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        this.setState({ randomCard: json });
+      });
   }
 
   render() {
@@ -41,13 +39,14 @@ class App extends Component {
       return null;
     }
 
-return (
-    <div>
-      <h1>Welcome to My MTG Site</h1>
-      <CardSearch />
-      <p>Explore and search for your favorite cards!</p>
-    </div>
-  );
+    return (
+      <div>
+        <h1>Welcome to My MTG Site</h1>
+        <CardSearch />
+        <p>Explore and search for your favorite cards!</p>
+      </div>
+    );
+  }
 }
 
 export default App;
